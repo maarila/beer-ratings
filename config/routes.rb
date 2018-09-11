54c2bc66cc19c03fcad09412f12168e9584c2700
Mika Äärilä <maarila@gmail.com>
2018-09-11 09:52:30 +0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
+  root 'breweries#index'
   resources :beers
   resources :breweries
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :ratings, only: [:index, :new, :create, :destroy]
 end
