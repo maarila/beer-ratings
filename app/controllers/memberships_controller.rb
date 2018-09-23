43a1a39ccf90e18251d78c177136cfda19ae5414
Mika Äärilä <maarila@gmail.com>
2018-09-23 18:41:01 +0300

    Refactor as per Rubocop suggestions

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -64,13 +64,14 @@ class MembershipsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_membership
-      @membership = Membership.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def membership_params
-      params.require(:membership).permit(:user_id, :beer_club_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_membership
+    @membership = Membership.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def membership_params
+    params.require(:membership).permit(:user_id, :beer_club_id)
+  end
 end
