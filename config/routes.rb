605cbead43c52a6c205276d340dbf1a1e2f4f02e
Mika Äärilä <maarila@gmail.com>
2018-09-23 18:30:44 +0300

    Update application

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  root 'breweries#index'
+  resources :memberships
+  get 'signup', to: 'users#new'
+  resources :users
   resources :beers
   resources :breweries
+  resources :beer_clubs
+  root 'breweries#index'
   resources :ratings, only: [:index, :new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 end
