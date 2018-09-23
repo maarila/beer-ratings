ba4c415a6c55eba8cd99be3c0952ebc24a80532d
Mika Äärilä <maarila@gmail.com>
2018-09-23 18:29:18 +0300

    Update models

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,8 +1,11 @@
 class Beer < ApplicationRecord
   include RatingAverage
 
+  validates :name, presence: true
+
   belongs_to :brewery
   has_many :ratings, dependent: :destroy
+  has_many :raters, through: :ratings, source: :user
 
   def average
     return 0 if ratings.empty?
--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,8 +1,16 @@
 class Brewery < ApplicationRecord
   include RatingAverage
 
+  validates :name, presence: true
+  validates :year, numericality: { only_integer: true }
+  validate :year_is_between_1040_and_current_year
+
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
+
+  def year_is_between_1040_and_current_year
+    errors.add(:year, "needs to be between 1040 and #{Time.now.year}") if year < 1040 || year > Time.now.year
+  end
 
   def print_report
     puts name
