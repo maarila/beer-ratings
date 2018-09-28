e628a9bebfe09840ddb53bda5eac1b73c5ec36a5
Mika Äärilä <maarila@gmail.com>
2018-09-28 15:46:24 +0300

    Add method and tests for favourite beer

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,12 +28,38 @@ RSpec.describe User, type: :model do
     expect(User.count).to eq(0)
   end
 
+  describe "favorite beer" do
+    let(:user) { FactoryBot.create(:user) }
+
+    it "has method for determining the favorite beer" do
+      expect(user).to respond_to(:favorite_beer)
+    end
+
+    it "without ratings does not have a favorite beer" do
+      expect(user.favorite_beer).to eq(nil)
+    end
+
+    it "is the only rated if only one rating" do
+      beer = FactoryBot.create(:beer)
+      rating = FactoryBot.create(:rating, score: 20, beer: beer, user: user)
+
+      expect(user.favorite_beer).to eq(beer)
+    end
+
+    it "is the one with highest rating if several rated" do
+      create_beers_with_many_ratings( { user: user }, 10, 20, 15, 7, 9 )
+      best = create_beer_with_rating( { user: user }, 25 )
+
+      expect(user.favorite_beer).to eq(best)
+    end
+  end
+
   describe "with a proper password" do
     let(:user) { FactoryBot.create(:user) }
 
     it "is saved" do
-    expect(user).to be_valid
-    expect(User.count).to eq(1)
+      expect(user).to be_valid
+      expect(User.count).to eq(1)
     end
 
     it "and with two ratings, has the correct average rating" do
@@ -43,5 +69,17 @@ RSpec.describe User, type: :model do
       expect(user.ratings.count).to eq(2)
       expect(user.average_rating).to eq(15.0)
     end
+  end
+end
+
+def create_beer_with_rating(object, score)
+  beer = FactoryBot.create(:beer)
+  FactoryBot.create(:rating, beer: beer, score: score, user: object[:user] )
+  beer
+end
+
+def create_beers_with_many_ratings(object, *scores)
+  scores.each do |score|
+    create_beer_with_rating(object, score)
   end
 end
