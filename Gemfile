4f0173cfddf5d51bb9cfc32976459e712ae78218
Mika Äärilä <maarila@gmail.com>
2018-10-01 18:25:40 +0300

    Add HTTParty and places search

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'bcrypt', '~> 3.1.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
+gem 'httparty'
+
 group :production do
   gem 'pg'
   gem 'rails_12factor'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :memberships
   get 'signup', to: 'users#new'
+  get 'places', to: 'places#index'
+  post 'places', to: 'places#search'
   resources :users
   resources :beers
   resources :breweries
