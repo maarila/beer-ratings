c355714feb12ee585f68b1ec5948b9dbdb15bea0
Mika Äärilä <maarila@gmail.com>
2018-10-11 19:52:16 +0300

    Add active status to breweries

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,12 +1,15 @@
 class Brewery < ApplicationRecord
   include RatingAverage
 
+  has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
   validates :name, presence: true
   validates :year, numericality: { only_integer: true }
   validate :year_is_between_1040_and_current_year
 
-  has_many :beers, dependent: :destroy
-  has_many :ratings, through: :beers
+  scope :active, -> { where active: true }
+  scope :retired, -> { where active: [nil, false] }
 
   def year_is_between_1040_and_current_year
     errors.add(:year, "needs to be between 1040 and #{Time.now.year}") if year < 1040 || year > Time.now.year
@@ -21,5 +24,10 @@ class Brewery < ApplicationRecord
   def restart
     self.year = 2018
     puts "Changed year to #{year}"
+  end
+
+  def self.top(amount)
+    sorted_by_rating_in_desc_order = Brewery.all.sort_by{ |b| -(b.average_rating || 0) }
+    sorted_by_rating_in_desc_order[0...amount]
   end
 end
